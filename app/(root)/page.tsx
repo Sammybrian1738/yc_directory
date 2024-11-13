48712be965f4edf3732731a57243ad92bfc3cf38
sammybrian1738 <briansammy1997@gmail.com>
2024-11-13 16:42:35 +0300

    Initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Tour Startup, <br /> Connect With Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit ideas, Vote on pitches, and Get noticed in Virtual Competitions
+        </p>
+      </section>
+    </>
+  );
+}
